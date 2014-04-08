808d27f1eef6b65c0b1645c346748bc588df7a43
jvgjordi <jvgjordi@hotmail.com>
2014-04-08 13:07:08 +0200

    Afegir arxius

---
+++ Paquet.java
@@ -0,0 +1,47 @@
+package Paquet;
+
+/**
+ *
+ *Clase Paquets
+ *
+ *@author Jordi Vilella
+ */
+
+public class Paquet{
+
+	//Atribtus
+
+	private static int idCounter = 0;
+	private int id;
+	private int ocupacio;
+	//private int capacitat;
+
+	//Constructores
+	
+	public Paquet(){
+		id = idCounter;
+		idCounter = idCounter + 1;
+		ocupacio = 0;
+	}
+
+	//Metodes
+
+
+	//Consultores
+	public int obtenirId(){
+		return id;
+	}
+	
+	public int obtenirOcupacio(int id){
+		return ocupacio;
+	}
+	
+
+	//Modificadores
+
+	public void modificarOcupacio(int ocupacio){
+		this.ocupacio = ocupacio;
+	}
+}
+
+
