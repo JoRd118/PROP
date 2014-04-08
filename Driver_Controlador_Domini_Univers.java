e259695cce9c3b9b073e960e1ee9ed25a72f9f29
JoRd118 <jordiface18@gmail.com>
2014-04-08 18:24:59 +0200

    Controladors_Univers

--- Driver_Controlador_Domini_Univers.java
+++ Driver_Controlador_Domini_Univers.java
@@ -23,12 +23,57 @@ public class Driver_Controlador_Domini_Univers{
 	public static void main(String[] args){
 		String nomClase = "Controlador Univers";
 		System.out.println("Driver "+nomClase);
+		Scanner scan = new Scanner(System.in);
+		boolean out = false;
+		String nom;
 
+		menu();
 
+		while(!out{
+			try{
+				String op = scan.nextLine();
+				switch(op){
+					case "0":
+						out = true;
+						break;
+					case "1":
+						u = new Controlador_Domini_Univers();
+						break;
+					case "2":
+						nom = scan.nextLine();
+						u.altaUnivers(nom);
+						break;
+					case "3":
+						nom = scan.nextLine();
+						u.baixaUnivers(nom);
+					case "4":
+						nom = scan.nextLine();
+						String new_nom = scan.nextLine();
+						u.modificacioNomUnivers(nom, new_nom);
+						break;
+					case "5":
+						nom = scan.nextLine();
+						u.obtenirUnivers(nom);
+						break;
+					case "6":
+						nom = scan.nextLine();
+						u.obtenirIdUnivers(nom);
+						break;
+					case "7":
+						int id = scan.nextLine();
+						u.obtenirNomUnivers(id);
+						break;
+					case "8":
+						System.out.println(u.llistatUnivers());
+						break;
+					default:
+						System.out.println("Opcio no valida\n");
+						break;
+
+				}
+			}
+			catch(Exception e)System.out.println(e.getMessage());
+		}
+		scan.close();
 	}
-	
-
-
-
-
 }
