d633a1e0327a991d1e5d1d283e991237a80d862d
oriolbv <oriolbv@gmail.com>
2014-04-29 19:31:27 +0200

    Coordenades_Final_pres

--- Entrega2/EXE/Controlador_Domini_Planeta/Coordenades.java
+++ Entrega2/EXE/Controlador_Domini_Planeta/Coordenades.java
@@ -6,24 +6,34 @@ public class Coordenades {
     public int x;
     public int y;
     
-  
+    // pre: -
+    // post: Es creen unes coordenades per defecte  
     public Coordenades() {
         x = 0;
         y = 0;
     }
+
+    // pre: -
+    // post: Es creen unes coordenades x i y
     public Coordenades(int x, int y) {
        this.x = x;
        this.y = y;
     }
 
+    // pre: -
+    // post: Retorna la coordenada x d'unes coordenades
     public int obtenirCoordenadesX() {
         return x;
     }    
 
+    // pre: -
+    // post: Retorna la coordenada y d'unes coordenades
     public int obtenirCoordenadesY() {
         return y;
     }   
 
+    // pre: -
+    // post: Modifica les coordenades per x i y
     public void modificarCoordenades(int x, int y) {
         this.x = x;
         this.y = y;
