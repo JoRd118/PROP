ca15b408ec9a5c768c23ccdab8cc1e4cfce99a85
Endevil07 <claudicervello@MacBook-MacBook-Pro-de-Claudi.local>
2014-04-30 09:30:13 +0200

    Dades

---
+++ Entrega2/EXE/Controlador_Dades_Univers/Driver_Controlador_Dades_Univers.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.IOException;
+/**
+ *
+ *Driver Controlador Domini Classe Univers
+ *
+ *@author Claudi
+ */
+public class Driver_Controlador_Dades_Univers{
+    
+    private static Controlador_Dades_Univers t;
+    
+    public static void menu() {
+        System.out.println("Escolllir opción:");
+        System.out.println("\t 00) Salida.");
+        System.out.println("\t 01) public Controlador_Dades_Univers().");
+        System.out.println("\t 02) public ArrayList<String> readTextFile(String nomFitxer).");
+        System.out.println("\t 03) public void writeTextFile(String nomFitxer).");
+    }
+    
+    public static void main(String[] args) {
+        String nombreClase = "Controlador_Dades_Univers";
+        System.out.println("Driver "+nombreClase);
+        Scanner scan = new Scanner(System.in);
+        ArrayList<String> a = new ArrayList<String>();
+        boolean out = false;
+        t = new Controlador_Dades_Univers();
+        menu();
+        
+        while(!out){
+            try{
+                String op = scan.nextLine();
+                switch (op) {
+                    case "01":
+                        
+                         t = new Controlador_Dades_Univers();
+                        break;
+                        
+                    case "02":
+                        op = scan.nextLine();
+                        a = t.readTextFile(op);
+                        break;
+                        
+                    case "03":
+                        op = scan.nextLine();
+                        t.writeTextFile(op, a);
+                        break;
+                        
+                    case "00":
+                        out = true;
+                        break;
+                        
+                    default:
+                        System.out.println("Opció no valida.");
+                        break;
+                }
+            }catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            finally{
+                //S'executa SEMPRE.
+            }
+        }
+        scan.close();
+    }
+    
+}
--- Entrega2/EXE/Controlador_Domini_Paquet/Recurs.java
+++ Entrega2/EXE/Controlador_Domini_Paquet/Recurs.java
@@ -13,11 +13,17 @@ public class Recurs{
     private String nom;
 
     //Constructores
+    
+    //Pre: True.
+    //Post: Instancia de Recurs.
     public Recurs(){
         id = idCounter;
         idCounter = idCounter + 1;
         nom = "Recurs_"+id;
     }
+    
+    //Pre: True.
+    //Post: Instancia de Recurs.
     public Recurs(String nom){
         id = idCounter;
         idCounter = idCounter + 1;
@@ -26,16 +32,24 @@ public class Recurs{
     
     
     //Modificadores
+    
+    //Pre: True.
+    //Post: El nom del recurs queda modificat a que es passa per paramatre.
     public void modificarNomRecurs(String new_name){
         this.nom = new_name;
     }
     
     
     //Consultores
+    
+    //Pre: True.
+    //Post: Retorna el id del Recurs.
     public int obtenirId(){
         return this.id;
     }
     
+    //Pre: True.
+    //Post: Retorna el nom del Recurs.
     public String obtenirNom(){
         return nom;
     }
