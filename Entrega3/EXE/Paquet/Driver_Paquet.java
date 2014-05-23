c1c5a35e417735d53be4fa3503017638091fcfec
jvgjordi <jvgjordi@hotmail.com>
2014-05-23 12:40:35 +0200

    Modifacar el driver de paquet

--- Entrega3/EXE/Paquet/Driver_Paquet.java
+++ Entrega3/EXE/Paquet/Driver_Paquet.java
@@ -11,8 +11,13 @@ public class Driver_Paquet {
         System.out.println("\t 01) public void Paquet().");
         System.out.println("\t 02) public void Paquet(int id)");
         System.out.println("\t 03) public int obtenirId().");
-        System.out.println("\t 04) public int obtenirOcupacio().");
-        System.out.println("\t 05) public int modificarOcupacio(int ocupacio).");
+        System.out.println("\t 04) public void assignarRecurs(String nom, Recurs rec)");
+        System.out.println("\t 05) public void esborrarRecurs(String nom)");
+        System.out.println("\t 06) public boolean validarrecurs(String nom)");
+        System.out.println("\t 07) public Iterable<String> llistatRecursosIterator()");
+        System.out.println("\t 08) public String llistatRecursos()");
+        //System.out.println("\t 04) public int obtenirOcupacio().");
+        //System.out.println("\t 05) public int modificarOcupacio(int ocupacio).");
 	}
 
 	public static void main(String[] args) {
@@ -26,6 +31,8 @@ public class Driver_Paquet {
             try{
                 String op = scan.nextLine();
                 String num;
+                String nom;
+                Integer id_rec = 0;
                 switch (op) {
                         
                     case "01":
@@ -42,14 +49,30 @@ public class Driver_Paquet {
                         break;
 
                     case "04":
-                        System.out.println("L'ocupacio es: " + p.obtenirOcupacio());
+                        nom = scan.nextLine();
+                        Recurs aux = new Recurs(nom,id_rec);
+                        ++id_rec;
+                        p.assignarRecurs(nom,aux);
                         break;
                         
                     case "05":
-                        num = scan.nextLine();
-                        p.modificarOcupacio(Integer.parseInt(num));
+                        nom = scan.nextLine();
+                        p.esborrarRecurs(nom);
                         break;
-                        
+
+                    case "06":
+                        nom = scan.nextLine();
+                        System.out.println(p.validarrecurs(nom));
+                        break;
+
+                    case "07":
+                        System.out.println(p.llistatRecursosIterator());
+                        break;
+
+                    case "08":
+                        System.out.println(p.llistatRecursos());
+                        break;
+
                     case "00":
                         out = true;
                         break;
