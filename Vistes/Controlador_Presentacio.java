3401d7f31babd77f247b35c0659bdd75540a4b4e
Endevil07 <claudicervello@6.xarxa-10-83-110.eduroam.upc.edu>
2014-05-23 14:05:58 +0200

    Vistes

---
+++ Vistes/Controlador_Presentacio.java
@@ -0,0 +1,30 @@
+/**
+ *
+ *Classe Recurs
+ *
+ *@author Claudi
+ */
+
+public class Controlador_Presentacio{
+    private VistaGlobal v;
+    private Controlador_Domini_Recurs cr;
+    private Controlador_Domini_Paquet cp;
+    private Controlador_Domini_Planeta cpl;
+    private Controlador_Domini_Univers cu;
+    
+    public Controlador_Presentacio(){
+        v = new VistaGlobal();
+        v.pack();
+        v.setVisible(true);
+        //if(v == null) System.out.println("F");
+        //sv = new  SubVista(v);
+        //Instencia tots els controladors.
+        cr = new Controlador_Domini_Recurs();
+        cp = new Controlador_Domini_Paquet();
+        cpl = new Controlador_Domini_Planeta();
+        cu = new Controlador_Domini_Univers();
+    }
+    
+    
+
+}
